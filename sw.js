a9751ee8fafd674ffea3fcd9309da2370cad8eef
grahambryan <grahambryan@users.noreply.github.com>
2026-03-17 15:45:58 +0000

    deploy demo from 9df813b8ebe61ffc2fc7ae154d0ef256f36adc22 grahambryan/tcg-tracker@9df813b8ebe61ffc2fc7ae154d0ef256f36adc22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tcg-v1773761780644';
+const CACHE = 'tcg-v1773762357610';
 const SHELL = ['./', 'manifest.json', 'icon-192.svg', 'icon-512.svg'];
 
 self.addEventListener('install', e => {
